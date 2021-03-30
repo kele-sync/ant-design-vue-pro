0a41ee6441e74f42341579919d63d2dbe1d89c18
kele <chj.ymh@gmail.com>
2021-03-30 10:11:59 +0800

    简化路由

--- src/views/analyze/Map.vue
+++ src/views/analyze/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <page-header-wrapper>
+    <a-alert message="客户打开平台分享的名片并同意获取地址，获取的定位将显示在地图上" type="success" close-text="Close Now" />
     <a-card>
-
       <el-amap vid="amapDemo" style="height:680px" >
       </el-amap>
     </a-card>
